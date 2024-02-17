49743d387f668dea9f3ad6e270b955dd5266f450
Lily <112404773+Belywen@users.noreply.github.com>
2024-02-17 19:30:31 +0100

    Mep anim personnage

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 let canvas = document.getElementById("myCanvas")
 let ctx = canvas.getContext("2d")
-let gameSection = document.getElementById("canvas-container")
-canvas.width = gameSection.clientWidth
-canvas.height = gameSection.clientHeight
+let canvasContainerSection = document.getElementById("canvasContainer")
+canvas.width = canvasContainerSection.clientWidth
+canvas.height = canvasContainerSection.clientHeight
 
 // Event pour pressions sur clavier
 document.addEventListener("keydown", handleKeyPress)
 document.addEventListener("keyup", handleKeyRelease)
 
-//Event désignant les actions d'albert lorsque l'on clique sur une flèche
+//Event désignant les actions du personnage lorsque l'on clique sur une flèche
 function handleKeyPress(event) {    
     switch (event.key) {
         case "ArrowRight":
-            moveAlbertRight()
+            movePersoRight()
             break
         case "ArrowLeft":
-            moveAlbertLeft()
+            movePersoLeft()
             break
     }
 }
@@ -26,35 +26,30 @@ function handleKeyRelease(event) {
         case "ArrowRight":
         case "ArrowLeft":
             isArrowKeyPressed = false
-            drawAlbert()
+            drawPerso()
             break
     }
 }
 
 
 // Partie du personnage
-//Variables pour Albert
+
+//Variables pour le personnage
 let x = 50
 let y = 50
-let isAlbertDroite = true
-let isAlbertDroite1 = true
+let isPersoDroite = true
+let isPersoDroite1 = true
 let isArrowKeyPressed = false
-let albertImage = new Image()
-albertImage.src = "images/persos/01a_perso.png"
+let persoImage = new Image()
+persoImage.src = "images/personnages/01a_perso.png"
 
-//Permet d'attendre que l'image soit complétement chargée avant de dessiner Albert
-albertImage.onload = function () {
-    drawAlbert();
+//Permet d'attendre que l'image soit complétement chargée avant de dessiner le personnage
+persoImage.onload = function () {
+    drawPerso()
 }
 
-//Simulation d'objet
-function redSquare() {
-    ctx.fillStyle = "red"
-    ctx.fillRect(50, 50, 50, 50)
-}
-
-/*Albert qui marche vers la droite n1*/
-function albertDroite1() {
+/*Personnage qui marche vers la droite n1*/
+function persoDroite1() {
     let sx = 1
     let sy = 3
     let sWidth = 52 
@@ -62,11 +57,11 @@ function albertDroite1() {
     let dx = x 
     let dy = y 
 
-    ctx.drawImage(albertImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
+    ctx.drawImage(persoImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
 }
 
-/*Albert qui marche vers la droite n2*/
-function albertDroite2() {
+/*Personnage qui marche vers la droite n2*/
+function persoDroite2() {
     let sx = 77
     let sy = 1
     let sWidth = 47 
@@ -74,11 +69,11 @@ function albertDroite2() {
     let dx = x 
     let dy = y 
 
-    ctx.drawImage(albertImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
+    ctx.drawImage(persoImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
 }
 
-/*Albert qui marche vers la gauche n1*/
-function albertGauche1() {
+/*Personnage qui marche vers la gauche n1*/
+function persoGauche1() {
     let sx = 584 
     let sy = 289
     let sWidth = 56
@@ -86,11 +81,11 @@ function albertGauche1() {
     let dx = x 
     let dy = y 
 
-    ctx.drawImage(albertImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
+    ctx.drawImage(persoImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
 }
 
-/*Albert qui marche vers la gauche n2*/
-function albertGauche2() {
+/*Personnage qui marche vers la gauche n2*/
+function persoGauche2() {
     let sx = 513
     let sy = 286
     let sWidth = 43
@@ -98,11 +93,11 @@ function albertGauche2() {
     let dx = x     
     let dy = y 
 
-    ctx.drawImage(albertImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
+    ctx.drawImage(persoImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
 }
 
-/*Albert face*/
-function albertFace() {  
+/*Personnage face*/
+function persoFace() {  
     // Dessine seulement la partie de l'image délimitée par les coordonnées (sx, sy) et les dimensions (sWidth, sHeight)
     let sx = 136.144 // Coordonnée x du coin supérieur gauche de la région à extraire
     let sy = 136.283 // Coordonnée y du coin supérieur gauche de la région à extraire
@@ -111,64 +106,63 @@ function albertFace() {
     let dx = x // Coordonnée x où dessiner l'image sur le canevas
     let dy = y // Coordonnée y où dessiner l'image sur le canevas
 
-    ctx.drawImage(albertImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
+    ctx.drawImage(persoImage, sx, sy, sWidth, sHeight, dx, dy, sWidth, sHeight)
 }
 
 
 let prevX = x
 let prevY = y
 
-//Efface Albert à chaque mouvement
-function clearAlbert() {
-    let clearWidth = 50 // Largeur de la région d'Albert
+//Efface personnage à chaque mouvement
+function clearPerso() {
+    let clearWidth = 50 // Largeur de la région du personnage
     ctx.clearRect(prevX - clearWidth, 0, clearWidth + 70, canvas.height)
 }
 
-/*Fonctions pour faire bouger Albert*/
-function moveAlbertRight() {
-    clearAlbert()
+/*Fonctions pour faire bouger le personnage*/
+function movePersoRight() {
+    clearPerso()
     isArrowKeyPressed = true //Touche pressée
-    isAlbertDroite = true //Direction d'Albert
+    isPersoDroite = true //Direction du personnage
     prevX = x
     x += 20 
-    drawAlbert()
+    drawPerso()
 }
 
-function moveAlbertLeft() { 
-    clearAlbert()
+function movePersoLeft() { 
+    clearPerso()
     isArrowKeyPressed = true //Touche pressée
-    isAlbertDroite = false // Direction d'Albert
+    isPersoDroite = false // Direction du personnage
     prevX = x
     x -= 20
-    drawAlbert()
+    drawPerso()
 }
 
-//Fonction qui dessine Albert
-function drawAlbert() {
-    clearAlbert()
+//Fonction qui dessine le personnage
+function drawPerso() {
+    clearPerso()
     if (isArrowKeyPressed) {
-        if (isAlbertDroite) { 
-            if (isAlbertDroite1) {
-                albertDroite1();
+        if (isPersoDroite) { 
+            if (isPersoDroite1) {
+                persoDroite1();
             } else {
-                albertDroite2();
+                persoDroite2();
             }
         } else {
-            if (isAlbertDroite1) {
-                albertGauche1();
+            if (isPersoDroite1) {
+                persoGauche1();
             } else {
-                albertGauche2();
+                persoGauche2();
             }
         }
-        isAlbertDroite1 = !isAlbertDroite1;
+        isPersoDroite1 = !isPersoDroite1;
     } else {
-        albertFace();
+        persoFace();
     }
 }
 
 function draw() {
-    drawAlbert()
-    redSquare()
+    drawPerso()
 }
 
 draw()
